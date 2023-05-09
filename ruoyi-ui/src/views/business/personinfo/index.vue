64a0bf39388390d9bbfe304e2724eeb3d0628161
BeckCui <734370179@qq.com>
2023-05-09 10:15:53 +0800

    xtwy-hrms init project

---
+++ ruoyi-ui/src/views/business/personinfo/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-container">
+    <el-row :gutter="20">
+      <el-col :span="4" :xs="24">
+        <div class="head-container">
+          人员信息
+        </div>
+        <div class="head-container">
+          人员信息
+        </div>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
+
+export default {
+  name: "User"
+};
+</script>
---
+++ ruoyi-ui/src/views/business/worktotal/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+    <el-row :gutter="20">
+      <el-col :span="4" :xs="24">
+        <div class="head-container">
+          工作量统计
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
+
+export default {
+  name: "User"
+};
+</script>
